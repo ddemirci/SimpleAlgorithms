b2ffe202692ebf81d87a604d50de8d92e85580e0
Doruk Demirci <dorukdemirci.boun@gmail.com>
2019-01-27 14:14:39 +0300

    Initial commit with recursive algorithms

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleAlgorithms
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            //Power examples
+            /*
+            Console.WriteLine("7th power of 2 is " + Algorithms.power(2, 7));
+            Console.WriteLine("4th power of 2 is " + Algorithms.power(2, 4));
+            Console.WriteLine("0th power of 2 is " + Algorithms.power(2, 0));
+            Console.WriteLine("1st power of 2 is " + Algorithms.power(2, 1));
+            Console.WriteLine("-3rd power of 2 is " + Algorithms.power(2, -3));
+            Console.WriteLine("3th power of 5 is " + Algorithms.power(5, 3));
+            */
+
+            //Number of digits examples
+            /*
+            Console.WriteLine("Number of digits in 431612 is " + Algorithms.numberOfDigits(431612));
+            Console.WriteLine("Number of digits in 123 is " + Algorithms.numberOfDigits(123));
+            Console.WriteLine("Number of digits in 5 is " + Algorithms.numberOfDigits(5));
+            Console.WriteLine("Number of digits in -431612 is " + Algorithms.numberOfDigits(-431612));
+            */
+
+            //Greatest common divisor examples
+            /*
+            Console.WriteLine("Greatest common divisor of 20 and 50 is " + Algorithms.gcd(20, 50));
+            Console.WriteLine("Greatest common divisor of 1 and 50 is " + Algorithms.gcd(1, 50));
+            Console.WriteLine("Greatest common divisor of 23 and 27 is " + Algorithms.gcd(23, 27));
+            Console.WriteLine("Greatest common divisor of 27 and 36 is " + Algorithms.gcd(27, 36));
+            */
+
+            //Palindrome examples
+            /*
+            Console.WriteLine("Is \"AXSDA\" palindrome: " + Algorithms.isPalindrome("AXSDA"));
+            Console.WriteLine("Is \"MOM\" palindrome: " + Algorithms.isPalindrome("MOM"));
+            Console.WriteLine("Is \"ABCDDCBA\" palindrome: " + Algorithms.isPalindrome("ABCDDCBA"));
+            Console.WriteLine("Is \"ABCDCBA\" palindrome: " + Algorithms.isPalindrome("ABCDCBA"));
+            Console.WriteLine("Is \"ABCDDcba\" palindrome: " + Algorithms.isPalindrome("ABCDDcba"));
+            */
+
+            //Factorial Examples
+            /*
+            Console.WriteLine("0! = " + Algorithms.factorial(0));
+            Console.WriteLine("1! = " + Algorithms.factorial(1));
+            Console.WriteLine("3! = " + Algorithms.factorial(3));
+            Console.WriteLine("5! = " + Algorithms.factorial(5));
+            Console.WriteLine("6! = " + Algorithms.factorial(6));
+            Console.WriteLine("7! = " + Algorithms.factorial(7));
+            */
+            //Fibonacci Examples
+            /*
+            Console.WriteLine("1st fibonacci number is " + RecursiveAlgorithms.fibonacci(1));
+            Console.WriteLine("2nd fibonacci number is " + RecursiveAlgorithms.fibonacci(2));
+            Console.WriteLine("3th fibonacci number is " + RecursiveAlgorithms.fibonacci(3));
+            Console.WriteLine("5th fibonacci number is " + RecursiveAlgorithms.fibonacci(5));
+            Console.WriteLine("10th fibonacci number is " + RecursiveAlgorithms.fibonacci(10));
+            */
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RecursiveAlgorithms.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleAlgorithms
+{
+    public static class RecursiveAlgorithms
+    {
+
+        public static double power(int a, int b) // returns a^b
+        {
+            if (b < 0)
+                return (double)1 / power(a, b * (-1));
+            else if (b == 0)
+                return 1;
+            else
+            {
+                double result = power(a, b / 2);
+                if (b % 2 == 1)
+                    return result * result * a;
+                else
+                    return result * result;
+            }
+        }
+
+        public static int numberOfDigits(int num) //returns the number of digits in num
+        {
+            if (num / 10 == 0)
+                return 1;
+            else
+                return 1 + numberOfDigits(num / 10);
+        }
+
+        public static int gcd(int a, int b) //returns the greatest common divisor of a & b
+        {
+            if (a == 1 || b == 1)
+                return 1;
+
+            while (a != b)
+            {
+                if (a > b)
+                    return gcd(a - b, b);
+                else
+                    return gcd(a, b - a);
+            }
+            return a;
+        }
+
+        public static bool isPalindrome(string str)
+        {
+            if (str.Length <= 1)
+                return true;
+            else if (String.Equals(str.Substring(0, 1), str.Substring(str.Length - 1), StringComparison.OrdinalIgnoreCase))
+                return isPalindrome(str.Substring(1, str.Length - 2));
+            else
+                return false;
+        }
+
+        public static long factorial(int number)
+        {
+            if (number < 0)
+                return 0;
+            else if (number <= 1)
+                return 1;
+            else
+                return number * factorial(number - 1);
+        }
+
+        public static int fibonacci(int term)
+        {
+            if (term == 1)
+                return 1;
+            else if (term == 2)
+                return 1;
+            else
+                return fibonacci(term - 1) + fibonacci(term - 2);
+        }
+    }
+}
